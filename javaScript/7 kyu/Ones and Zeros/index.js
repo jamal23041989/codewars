8e49e03167b9c454997e467c5eec3def68b097ec
jk <jk@MacBook-Air-jk.local>
2023-01-08 23:58:09 +0300

    Ones and Zeros

---
+++ javaScript/7 kyu/Ones and Zeros/index.js
@@ -0,0 +1,12 @@
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2)
+}
+
+console.log(binaryArrayToNumber([0, 0, 0, 1])) // ==> 1
+console.log(binaryArrayToNumber([0, 0, 1, 0])) //==> 2
+console.log(binaryArrayToNumber([0, 1, 0, 1])) // ==> 5
+console.log(binaryArrayToNumber([1, 0, 0, 1])) //==> 9
+console.log(binaryArrayToNumber([0, 0, 1, 0])) //==> 2
+console.log(binaryArrayToNumber([0, 1, 1, 0])) // ==> 6
+console.log(binaryArrayToNumber([1, 1, 1, 1])) //==> 15
+console.log(binaryArrayToNumber([1, 0, 1, 1])) // ==> 11
