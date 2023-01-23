e570a608b505390f0fd5ae03a3c67c3f705b64bc
Jamal Kurbanov <jamalkurbanov23041989@gmail.com>
2023-01-23 19:09:34 +0300

    Sum of a sequence

---
+++ javaScript/7 kyu/Sum of a sequence/index.js
@@ -0,0 +1,21 @@
+// 1
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) return 0
+  let sum = 0
+
+  for (let i = begin; i <= end; i += step) {
+    sum += i
+  }
+
+  return sum
+}
+
+// 2
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) return 0
+  return begin + sequenceSum(begin + step, end, step)
+}
+
+console.log(sequenceSum(2, 6, 2)) // 12
+console.log(sequenceSum(1, 5, 1)) // 15
+console.log(sequenceSum(1, 5, 3)) // 5
