0a0981cfc1014ca50c638babba80044b5b904136
jk <jamalkurbanov23041989@gmail.com>
2023-02-05 12:53:21 +0300

    You Can't Code Under Pressure #1

---
+++ python/8 kyu/You Can't Code Under Pressure #1/README.md
@@ -0,0 +1,7 @@
+## en
+
+Code as fast as you can! You need to double the integer and return it.
+
+## ru
+
+Код так быстро, как вы можете! Вам нужно удвоить целое число и вернуть его.
---
+++ python/8 kyu/You Can't Code Under Pressure #1/index.py
@@ -0,0 +1,9 @@
+def double_integer(i):
+    return i * 2
+
+
+print(double_integer(2)) # 4
+print(double_integer(4)) # 8
+print(double_integer(-10)) # -20
+print(double_integer(0)) # 0
+print(double_integer(100)) # 200
